ba45f5eaaf53d65dfade91bd17e7b5c38fac2a77
yunhwan <yunhwan.choo00@gmail.com>
2023-02-14 16:33:18 +0900

    make table

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,47 @@
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [coinArray, setCoinArray] = useState();
 
   useEffect(()=>{
     const getCoin = async () => {
       await axios.get('http://localhost:5000')
       .then(({data}) => {
         console.log(data);
+        setCoinArray(data);
       })
       .catch((error) => {
-        console.log(error);
+        console.error(error);
       })
     }
     getCoin();
   },[])
 
+  const rows = coinArray.map((row, index) =>
+    <tr key={index}>
+      <td>{index+1}</td>
+      <td>{row.id}</td>
+      <td>{row.max_supply}</td>
+    </tr>
+  )
+
   return (
     <div>
       <h1>hello world</h1>
-      <button>new button</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Ranking</th>
+            <th>Name</th>
+            <th>Max_supply</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
     </div>
   );
 }
